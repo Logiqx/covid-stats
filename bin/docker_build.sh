829eb980334cdbfbe2f743b4adfa3af805fdea81
Logiqx <software@mikeg.me.uk>
2021-01-05 21:14:48 +0000

    Add NHS download script

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -11,5 +11,8 @@ DOCKER_BUILDKIT=1 docker build . --file Dockerfile --build-arg LOGIQX_DEBUG -t $
 # Download PHE data
 run_py_script phe_download.py
 
+# Download NHS data
+run_py_script nhs_download.py
+
 # Docker Tag
 docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
