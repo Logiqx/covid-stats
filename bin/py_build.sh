fe12491209352f20204545ba193dedc650006f84
Logiqx <software@mikeg.me.uk>
2021-01-11 18:05:33 +0000

    Fix issue in py build script

--- bin/py_build.sh
+++ bin/py_build.sh
@@ -11,7 +11,7 @@ DOCKER_BUILDKIT=1 docker build . --file Dockerfile --build-arg LOGIQX_DEBUG -t $
 # Copy the library scripts
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/common_core.py >$PROJ_DIR/python/common_core.py
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/phe_core.py >$PROJ_DIR/python/phe_core.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/phe_core.py >$PROJ_DIR/python/ons_core.py
+docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/ons_core.py >$PROJ_DIR/python/ons_core.py
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/nhs_core.py >$PROJ_DIR/python/nhs_core.py
 
 # Clear pycache
