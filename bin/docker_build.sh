d426e6c125de1f742224147333bc673f618ee6c9
Logiqx <software@mikeg.me.uk>
2021-01-19 22:29:39 +0000

    Use Debian slim because numpy needs more work for Alpine

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -6,10 +6,10 @@ IMAGE_NAME=$PROJ_NAME
 IMAGE_TAG=$(git rev-parse --short=12 HEAD)
 
 # Docker Build
-DOCKER_BUILDKIT=1 docker build . --file Dockerfile --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_TAG
+DOCKER_BUILDKIT=1 docker build . --file Dockerfile-slim --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_TAG
 
 # Download PHE data
-run_py_script phe_download.py
+#run_py_script phe_download.py
 
 # Download NHS data
 run_py_script xlsx_download.py
