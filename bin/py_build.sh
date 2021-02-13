13a2da95942ec04e36efdb32d63f403ca8272885
Logiqx <software@mikeg.me.uk>
2021-02-13 10:03:31 +0000

    Rename population module to ons_population

--- bin/py_build.sh
+++ bin/py_build.sh
@@ -14,9 +14,9 @@ docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/phe_core.py >$
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/phe_download.py >$PROJ_DIR/python/phe_download.py
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/ons_core.py >$PROJ_DIR/python/ons_core.py
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/ons_download.py >$PROJ_DIR/python/ons_download.py
+docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/ons_population.py >$PROJ_DIR/python/ons_population.py
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/nhs_core.py >$PROJ_DIR/python/nhs_core.py
 docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/nhs_download.py >$PROJ_DIR/python/nhs_download.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/population.py >$PROJ_DIR/python/population.py
 
 # Clear pycache
 rm -f $PROJ_DIR/python/__pycache__/*.*.pyc
