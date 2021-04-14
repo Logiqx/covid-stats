2e04028f111a37cf7f1d2044013e09fc4e2fb672
Logiqx <software@mikeg.me.uk>
2021-04-14 13:23:02 +0100

    Generate reports for regions

--- bin/phe_charts.sh
+++ bin/phe_charts.sh
@@ -4,5 +4,9 @@
 # Refresh PHE Charts
 run_py_script phe_charts.py
 
+# Refresh PHE Regional Charts
+run_py_script phe_charts_regions.py
+
 # Compress Images
 optipng -o2 docs/daily-trends/nation/england/*/*png
+optipng -o2 docs/daily-trends/regions/*/*png 
--- bin/phe_refresh.sh
+++ bin/phe_refresh.sh
@@ -10,5 +10,9 @@ run_py_script phe_convert.py
 # Refresh PHE Charts
 run_py_script phe_charts.py
 
+# Refresh PHE Regional Charts
+run_py_script phe_charts_regions.py
+
 # Compress Images
-optipng -o2 docs/daily-trends/nation/england/*/*png
+optipng -o2 docs/daily-trends/nation/england/*/*png 
+optipng -o2 docs/daily-trends/regions/*/*png 
