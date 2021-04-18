f1cbc522faadbf72b3a6ead1cb3ccf5e89ad0209
Logiqx <software@mikeg.me.uk>
2021-04-18 19:53:19 +0100

    Optimise nations charts

--- bin/phe_charts.sh
+++ bin/phe_charts.sh
@@ -9,4 +9,5 @@ run_py_script phe_charts_regions.py
 
 # Compress Images
 optipng -o2 docs/daily-trends/nation/england/*/*png
+optipng -o2 docs/daily-trends/nations/*/*png 
 optipng -o2 docs/daily-trends/regions/*/*png 
--- bin/phe_refresh.sh
+++ bin/phe_refresh.sh
@@ -15,4 +15,5 @@ run_py_script phe_charts_regions.py
 
 # Compress Images
 optipng -o2 docs/daily-trends/nation/england/*/*png 
+optipng -o2 docs/daily-trends/nations/*/*png 
 optipng -o2 docs/daily-trends/regions/*/*png 
