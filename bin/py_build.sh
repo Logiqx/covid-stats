26d39e5e5213865b64fa243913741a1ddd4e83a8
Logiqx <software@mikeg.me.uk>
2021-11-08 11:48:23 +0000

    Remove hard-coded image details from build script

--- bin/py_build.sh
+++ bin/py_build.sh
@@ -9,14 +9,14 @@ IMAGE_TAG=tmp
 DOCKER_BUILDKIT=1 docker build . --file Dockerfile-slim --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_TAG
 
 # Copy the library scripts
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/common_core.py >$PROJ_DIR/python/common_core.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/phe_core.py >$PROJ_DIR/python/phe_core.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/phe_download.py >$PROJ_DIR/python/phe_download.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/ons_core.py >$PROJ_DIR/python/ons_core.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/ons_download.py >$PROJ_DIR/python/ons_download.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/ons_population.py >$PROJ_DIR/python/ons_population.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/nhs_core.py >$PROJ_DIR/python/nhs_core.py
-docker run --rm --entrypoint cat covid-stats:tmp $WORK_DIR/python/nhs_download.py >$PROJ_DIR/python/nhs_download.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/common_core.py >$PROJ_DIR/python/common_core.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/phe_core.py >$PROJ_DIR/python/phe_core.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/phe_download.py >$PROJ_DIR/python/phe_download.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/ons_core.py >$PROJ_DIR/python/ons_core.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/ons_download.py >$PROJ_DIR/python/ons_download.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/ons_population.py >$PROJ_DIR/python/ons_population.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/nhs_core.py >$PROJ_DIR/python/nhs_core.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/nhs_download.py >$PROJ_DIR/python/nhs_download.py
 
 # Clear pycache
 rm -f $PROJ_DIR/python/__pycache__/*.*.pyc
